60a7e5652edb4b414250bf58b9823f062205fc0f
Mohit <76819254+mohit11R@users.noreply.github.com>
2021-02-11 15:45:05 +0530

    Add files via upload

---
+++ Javascript_Projects/Project_1/Project_2/script.js
@@ -0,0 +1,6 @@
+function genCat(){
+    var image = document.createElement("img");
+    var div = document.getElementById("flex-cat");
+    image.src = "https://images.pexels.com/photos/5004611/pexels-photo-5004611.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+    div.appendChild(image);
+}
